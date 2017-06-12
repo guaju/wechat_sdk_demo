5ca8abb8c3236732999557db1faa24d9f15ab5b9
guaju <guaju@126.com>
2017-06-12 23:38:07 +0800

    share a wechat pay demo cause the official demo is too hard to be used

---
+++ app/src/main/java/com/guaju/wechat_sdk_demo/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,57 @@
+package com.guaju.wechat_sdk_demo.wxapi;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.guaju.wechat_sdk_demo.Constants;
+import com.guaju.wechat_sdk_demo.R;
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+
+public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
+	
+	private static final String TAG = "MicroMsg.SDKSample.WXPayEntryActivity";
+	
+    private IWXAPI api;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pay_result);
+        
+    	api = WXAPIFactory.createWXAPI(this, Constants.APP_ID);
+        api.handleIntent(getIntent(), this);
+    }
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		setIntent(intent);
+        api.handleIntent(intent, this);
+	}
+
+	@Override
+	public void onReq(BaseReq req) {
+	}
+
+	@Override
+	public void onResp(BaseResp resp) {
+		Log.d(TAG, "onPayFinish, errCode = " + resp.errCode);
+
+		if (resp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(R.string.app_tip);
+			builder.setMessage(getString(R.string.pay_result_callback_msg, String.valueOf(resp.errCode)));
+			builder.show();
+		}
+	}
+}
